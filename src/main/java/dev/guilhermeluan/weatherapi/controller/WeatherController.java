35aa5023d07cbc902d7fb0acbcb5444b3497a533
Guilherme Luan <guilhermeluan@pm.me>
2025-02-25 22:02:18 -0300

    feat: send visual crossing get request

--- src/main/java/dev/guilhermeluan/weatherapi/controller/WeatherController.java
+++ src/main/java/dev/guilhermeluan/weatherapi/controller/WeatherController.java
@@ -1,5 +1,8 @@
 package dev.guilhermeluan.weatherapi.controller;
 
+import dev.guilhermeluan.weatherapi.infra.client.SendVisualCrossing;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/weather")
 public class WeatherController {
 
+    @Value("${api.key.visual_crossing}")
+    private String apiKey;
+    private SendVisualCrossing sendVisualCrossing;
+
+    public WeatherController(SendVisualCrossing sendVisualCrossing) {
+        this.sendVisualCrossing = sendVisualCrossing;
+    }
+
     @GetMapping
     public String getWeather() {
-        return "It's sunny today!";
+        return sendVisualCrossing.getWeather("Brazil", apiKey, "json").toString();
     }
 }
---
+++ src/main/java/dev/guilhermeluan/weatherapi/infra/client/SendVisualCrossing.java
@@ -0,0 +1,17 @@
+package dev.guilhermeluan.weatherapi.infra.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "visual-crossing",
+        url = "${api.base_url.visual_crossing}")
+public interface SendVisualCrossing {
+    @GetMapping("/{location}/today")
+    Object getWeather(
+            @PathVariable("location") String location,
+            @RequestParam("key") String apiKey,
+            @RequestParam(value = "contentType", defaultValue = "json", required = false) String contentType
+    );
+}
